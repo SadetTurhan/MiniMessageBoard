60cb1590e9e7e095c9793df6843d1e0a134cb220
Sadet Turhan <msadetturhan1@gmail.com>
2023-07-05 14:37:34 +0300

    started form

--- routes/messages.js
+++ routes/messages.js
@@ -1,24 +1,33 @@
 const express = require("express")
 const router = express.Router()
-
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+ ];
 
 router.get("/",(req,res) =>{
-    const messages = [
-   {
-     text: "Hi there!",
-     user: "Amando",
-     added: new Date()
-   },
-   {
-     text: "Hello World!",
-     user: "Charles",
-     added: new Date()
-   }
-];
+  
     res.render("index", { title: "Mini Messageboard", messages: messages })
 })
 
 router.get("/new",(req,res) =>{
     res.render("new")
 })
+
+router.post("/new",(req,res) => {
+    const userName = req.body.userName;
+    const messageText = req.body.messageText;
+    console.log(userName)
+    console.log(messageText)
+    res.redirect('/');
+})
+
 module.exports = router
