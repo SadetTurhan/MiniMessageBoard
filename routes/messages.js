3f9a41acd54023e3b577d6e70047460ea300cbe6
Sadet Turhan <msadetturhan1@gmail.com>
2023-07-05 14:41:40 +0300

    should work

--- routes/messages.js
+++ routes/messages.js
@@ -12,22 +12,27 @@ const messages = [
       added: new Date()
     }
  ];
-
 router.get("/",(req,res) =>{
-  
     res.render("index", { title: "Mini Messageboard", messages: messages })
 })
 
 router.get("/new",(req,res) =>{
     res.render("new")
 })
-
-router.post("/new",(req,res) => {
+router.post("/new", (req, res) => {
     const userName = req.body.userName;
     const messageText = req.body.messageText;
-    console.log(userName)
-    console.log(messageText)
-    res.redirect('/');
-})
-
-module.exports = router
+    const newMessage = {
+      text: messageText,
+      user: userName,
+      added: new Date(),
+    };
+    messages.push(newMessage);
+  
+    res.redirect("/");
+  });
+  module.exports = {
+    router,
+    messages,
+  };
+  
