f00d8191c85709f01b0994ec81aef7dd273d1e31
Sadet Turhan <msadetturhan1@gmail.com>
2023-07-05 14:14:03 +0300

    started rendering

--- routes/messages.js
+++ routes/messages.js
@@ -1,9 +1,23 @@
 const express = require("express")
 const router = express.Router()
 
+
 router.get("/",(req,res) =>{
-    res.render("index")
+    const messages = [
+   {
+     text: "Hi there!",
+     user: "Amando",
+     added: new Date()
+   },
+   {
+     text: "Hello World!",
+     user: "Charles",
+     added: new Date()
+   }
+];
+    res.render("index", { title: "Mini Messageboard", messages: messages })
 })
+
 router.get("/new",(req,res) =>{
     res.render("new")
 })
